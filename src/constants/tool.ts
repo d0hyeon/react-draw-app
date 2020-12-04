82a80a6547b650be001b5b6ebdd649e2b76b9562
kdh <do.hyeon.lioncco.com>
2020-12-04 17:56:50 +0900

    feat:  상수파일 추가

---
+++ src/constants/tool.ts
@@ -0,0 +1,8 @@
+import { Tools } from "src/types/tool";
+
+export const TOOL_CONFIG: Tools = {
+    pen: {
+      isColor: true,
+      icon: 'https://www.flaticon.com/svg/static/icons/svg/2050/2050922.svg'
+    }
+  }
