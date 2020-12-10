4c5fe09100878a6e26a0134124305197b2bdb2c9
d0hyeon <ehgus6887@gmail.com>
2020-12-10 17:08:25 +0900

    feat: tool config > 지우개 툴 추가

--- src/constants/tools.ts
+++ src/constants/tools.ts
@@ -1,5 +1,6 @@
 import { Tool } from 'src/types/tool';
 import * as Brash from 'src/components/tools/Brash';
+import BrashNavigate from 'src/components/tools/BrashNavigate';
 
 export type Tools = {
   [key: string]: Tool;
@@ -9,6 +10,15 @@ export const toolConfigs: Tools = {
   [Brash.key]: {
     icon: Brash.icon,
     Component: Brash.default,
+    Navigate: BrashNavigate,
+  },
+  eraser: {
+    icon: 'https://img.icons8.com/ios-filled/344/ffffff/eraser.png',
+    Component: Brash.default,
+    Navigate: BrashNavigate,
+    props: {
+      globalCompositeOperation: 'destination-out',
+    },
   },
 };
 
