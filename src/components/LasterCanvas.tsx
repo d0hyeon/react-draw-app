41d4795e117af1ac4ebcf05d6bbaa8bbf051cdd2
d0hyeon <ehgus6887@gmail.com>
2020-12-15 19:07:55 +0900

    feat: 드로잉 전용 캔버스 추가

---
+++ src/components/LasterCanvas.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { useRecoilValue } from 'recoil';
+import { layer } from 'src/atoms/layer';
+import { tool } from 'src/atoms/tool';
+import { toolConfigs } from 'src/constants/tools';
+import { configSelector } from 'src/atoms/config';
+
+const LasterCanvas = () => {
+  const { currentLayerId } = useRecoilValue(layer);
+  const toolState = useRecoilValue(tool);
+  const configState = useRecoilValue(configSelector);
+
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const curreutTool = toolConfigs[toolState.tool];
+
+  return (
+    <>
+      <StyledCanvas
+        ref={canvasRef}
+        width={configState.width}
+        height={configState.height}
+      />
+      <curreutTool.Component
+        id={currentLayerId}
+        canvasRef={canvasRef}
+        toolState={toolState}
+        {...(curreutTool.props ?? {})}
+      />
+    </>
+  );
+};
+
+export default LasterCanvas;
+
+const StyledCanvas = styled.canvas`
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 99999;
+`;
