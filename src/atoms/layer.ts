723063517af9370943dc546c98c430e6472aff8c
d0hyeon <ehgus6887@gmail.com>
2020-12-15 19:07:36 +0900

    refactor: layter atom 데이터 구조 수정

--- src/atoms/layer.ts
+++ src/atoms/layer.ts
@@ -2,34 +2,41 @@ import { atomFamily, atom } from 'recoil';
 import { nanoid } from 'nanoid';
 import { ID } from 'src/types/common';
 
+export interface ContextState {
+  strokeX: number;
+  strokeY: number;
+  strokeWidth: number;
+  strokeHeight: number;
+  background?: string;
+  imageData: ImageData | null;
+}
+
 export interface LayerEntity {
+  id: ID;
   title: string;
   isDisplay: boolean;
   isLock: boolean;
-  canvas?: HTMLCanvasElement | null;
-  context: {
-    strokeX: number;
-    strokeY: number;
-    strokeWidth: number;
-    strokeHeight: number;
-    background?: string;
-  };
+  canvas: HTMLCanvasElement | null;
+  contextState: ContextState;
 }
 
-interface Layer {
+export interface Layer {
   currentLayerId: ID;
   layers: ID[];
 }
 
 const INITIAL_LAYER_ENTITY_STATE: LayerEntity = {
+  id: '',
   title: '',
   isDisplay: true,
   isLock: false,
-  context: {
+  canvas: null,
+  contextState: {
     strokeX: 0,
     strokeY: 0,
     strokeWidth: 0,
     strokeHeight: 0,
+    imageData: null,
     background: '',
   },
 };
@@ -43,12 +50,14 @@ export const layerEntity = atomFamily<LayerEntity, ID>({
   key: 'layerEntity',
   default: (id) => {
     const isDefaultLayer = id === DEFAULT_LAYER_ID;
+
     return {
       ...INITIAL_LAYER_ENTITY_STATE,
-      title: isDefaultLayer ? 'background' : `layer-${id}`,
+      id,
+      title: isDefaultLayer ? 'Background' : `Layer-${id}`,
       ...(isDefaultLayer && {
-        context: {
-          ...INITIAL_LAYER_ENTITY_STATE.context,
+        contextState: {
+          ...INITIAL_LAYER_ENTITY_STATE.contextState,
           background: '#fff',
         },
       }),
