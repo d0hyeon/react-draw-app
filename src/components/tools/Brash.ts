dd02879b8d85591a5756ea982f7e4cb013b5741f
d0hyeon <ehgus6887@gmail.com>
2020-12-15 19:09:26 +0900

    refactor: 드로잉 전용 컴포넌트가 생성되어 stroke 시 타겟 canvas 찾아 적용

--- src/components/tools/Brash.ts
+++ src/components/tools/Brash.ts
@@ -2,8 +2,9 @@ import React from 'react';
 import throttle from 'lodash/throttle';
 import { ToolComponentProps } from 'src/types/tool';
 import { StrokeEvent } from 'src/types/common';
-import { tool } from 'src/atoms/tool';
+// import { tool } from 'src/atoms/tool';
 import { useRecoilState } from 'recoil';
+import { layerEntity } from 'src/atoms/layer';
 
 const DEFAULT_CONTEXT_PROPERTIES = {
   globalCompositeOperation: 'source-over',
@@ -14,9 +15,9 @@ declare global {
   interface CanvasRenderingContext2D extends StrokeEvent {}
 }
 
-const Brash: React.FC<ToolComponentProps> = ({ canvasRef, ...props }) => {
-  const context = canvasRef.current?.getContext?.('2d') || null;
-  const [toolState] = useRecoilState(tool);
+const Brash: React.FC<ToolComponentProps> = ({ id, canvasRef, toolState, ...props }) => {
+  const [layerState, setLayerState] = useRecoilState(layerEntity(id));
+  const context = layerState.canvas?.getContext?.('2d') || null;
 
   const onMouseDown = React.useCallback(
     ({ offsetX, offsetY }) => {
@@ -25,51 +26,52 @@ const Brash: React.FC<ToolComponentProps> = ({ canvasRef, ...props }) => {
       context.lineWidth = toolState.lineWidth;
       context.moveTo(offsetX, offsetY);
 
-      const contextEvent = new CustomEvent<CanvasRenderingContext2D>(
-        'contextChange',
-        {
-          detail: context,
-        },
-      );
-      canvasRef.current.dispatchEvent(contextEvent);
+      const contextEvent = new CustomEvent<CanvasRenderingContext2D>('contextChange', {
+        detail: context,
+      });
+      layerState.canvas.dispatchEvent(contextEvent);
       canvasRef.current.addEventListener('mousemove', throttledOnMouseMove);
     },
-    [canvasRef, context, toolState.color, toolState.lineWidth],
+    [context, layerState.canvas, toolState.color, toolState.lineWidth],
   );
 
   const onMouseMove = React.useCallback(
     ({ offsetX, offsetY }) => {
-      let { strokeX = [0, 0], strokeY = [0, 0] } = context;
+      let { strokeX, strokeY, strokeWidth, strokeHeight } = layerState.contextState;
 
-      if (strokeX[0] === 0) {
-        strokeX[0] = offsetX;
-      }
-      if (strokeY[0] === 0) {
-        strokeY[0] = offsetY;
-      }
-
-      if (strokeX[0] > offsetX) {
-        strokeX[0] = offsetX;
-      } else if (strokeX[1] < offsetX) {
-        strokeX[1] = offsetX;
-      }
-      if (strokeY[0] > offsetY) {
-        strokeY[0] = offsetY;
-      } else if (strokeY[1] < offsetY) {
-        strokeY[1] = offsetY;
+      if (strokeX === 0) {
+        strokeX = offsetX;
+      } else if (strokeX > offsetX) {
+        strokeX = offsetX;
+      } else if (strokeWidth < offsetX) {
+        strokeWidth = offsetX;
       }
 
-      context.strokeX = strokeX;
-      context.strokeY = strokeY;
+      if (strokeY === 0) {
+        strokeY = offsetY;
+      } else if (strokeY > offsetY) {
+        strokeY = offsetY;
+      } else if (strokeHeight < offsetY) {
+        strokeHeight = offsetY;
+      }
+
+      setLayerState((prev) => ({
+        ...prev,
+        contextState: {
+          ...prev.contextState,
+          strokeX,
+          strokeY,
+          strokeWidth,
+          strokeHeight,
+        },
+      }));
+
       context.lineTo(offsetX, offsetY);
       context.stroke();
     },
-    [canvasRef, context],
+    [canvasRef, context, layerState],
   );
-  const throttledOnMouseMove = React.useMemo(
-    () => throttle(onMouseMove, 1000 / 60),
-    [onMouseMove],
-  );
+  const throttledOnMouseMove = React.useMemo(() => throttle(onMouseMove, 1000 / 60), [onMouseMove]);
 
   const onMouseUp = React.useCallback(() => {
     canvasRef.current.removeEventListener('mousemove', throttledOnMouseMove);
@@ -105,7 +107,6 @@ const Brash: React.FC<ToolComponentProps> = ({ canvasRef, ...props }) => {
 };
 
 export const key = 'brash';
-export const icon =
-  'https://img.icons8.com/ios-filled/344/ffffff/marker-pen.png';
+export const icon = 'https://img.icons8.com/ios-filled/344/ffffff/marker-pen.png';
 
 export default React.memo(Brash);
