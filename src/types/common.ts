11c2d270781278943385dee3e85185d217c3fd3c
d0hyeon <ehgus6887@gmail.com>
2020-12-16 15:33:33 +0900

    feat: StrokeEvent 인터페이스 롤백

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,8 +1,12 @@
+export type ID = string | number;
+
 export interface Config {
   width: number;
   height: number;
   title: string;
 }
+
+export type Merge<F, S> = F & S;
 
 export type ActionPayload<P> = {
   type: string;
@@ -12,15 +16,8 @@ export type ActionPayload<P> = {
 export type Reducer<S> = (state: S, payload: ActionPayload<S>) => S;
 
 export interface StrokeEvent {
-  strokeY: number;
   strokeX: number;
+  strokeY: number;
   strokeWidth: number;
   strokeHeight: number;
 }
-
-/************
- * @TODO StrokeEvent
- * [[x,y], [x,y], [x,y], [x,y]]
- * or
- * [{x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}]
- */
